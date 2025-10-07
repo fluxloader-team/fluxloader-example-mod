e296da73707225a9f2466af88fd22aa087b371ad
Electric131 <39623923+Electric131@users.noreply.github.com>
2025-10-07 16:45:09 -0500

    Add prettier workflow

--- entry.worker.js
+++ entry.worker.js
@@ -1,7 +1,7 @@
 log("info", "examplemod", "Worker entrypoint loaded");
 
 fluxloaderAPI.events.on("fl:worker-initialized", () => {
-    const index = fluxloaderAPI.gameInstanceState.environment?.threadMeta?.startingIndex;
-    log("info", "examplemod", `Worker initialized with index: ${index}`);
-    fluxloaderAPI.sendGameMessage("examplemod:gamemsg", index, "Hello from the worker!");
+	const index = fluxloaderAPI.gameInstanceState.environment?.threadMeta?.startingIndex;
+	log("info", "examplemod", `Worker initialized with index: ${index}`);
+	fluxloaderAPI.sendGameMessage("examplemod:gamemsg", index, "Hello from the worker!");
 });
